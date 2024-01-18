b6cabc21943cea90573cf6eea8ec3634076bb342
prerana211 <preranamainali49@gmail.com>
2024-01-18 16:49:27 -0500

    Modified both files in the repc clone
    Deleted a print() statement from script1.py
    added one print() statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print lines from Zen Of python
 print("Beautiful is better than ugly.")
-print("Simple is better than complex.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # print lines from Zen Of python
 print("Flat is better than nested.")
 print("Readability counts.")
+print("Namespaces are one honking great idea -- lets do more of these!")
